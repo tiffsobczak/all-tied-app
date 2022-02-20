2dae24c2c5fab631ad241116e8cda8a0a655f322
Alfrugo <adiez@mtm-inc.net>
2022-02-19 19:49:20 -0600

    Registration with profile works

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,58 +1,64 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
-  
-  }
-  
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-
-  async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-      //check the response status
-      if (response.ok) {
-          console.log('success');
-      } else {
-          alert(response.statusText);
-      }
-    }
-  }
-
 async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/profiles/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
   }
-  
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+}
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  const bio = document.querySelector('#bio-signup').value.trim();
+  const age = document.querySelector('#age-signup').value.trim();
+  const shift = document.querySelector('#shift-signup').value.trim();
+  const contact = document.querySelector('#contact-signup').value.trim();
+  const imageurl = document.querySelector('#imageurl-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/profiles', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+        bio,
+        age,
+        shift,
+        contact,
+        imageurl
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      // alert(response.statusText);
+      window.alert('Enter the right data dude')
+    }
+  }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
